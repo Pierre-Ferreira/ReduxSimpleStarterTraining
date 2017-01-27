6363523ca9f3053602d2696b2a5804b541256f7a
Pierre Ferreira <pierre@tektite.biz>
2017-01-27 09:24:51 +0200

    Update3

--- src/components/comment_list.js
+++ src/components/comment_list.js
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import { connect } from 'react-redux' // Connect turns the component into a container.
+// A container is a React component that has access to application level state through Redux.
 // Functional component.
 const CommentList = (props) => {
+  const list = props.comments.map(comment => <li key={comment}>{comment}</li>);
   return (
-    <ul className='comment-list'>
-      <li>COMMENTLIST</li>
-    </ul>
+    <ul className='comment-list'>{list}</ul>
   )
 }
-export default CommentList;
+
+function mapStateToProps(state) {
+  return  { comments: state.comments };
+}
+export default connect(mapStateToProps)(CommentList); // Connect turns the component into a container.
